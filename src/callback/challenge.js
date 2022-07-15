74f9390188a21f15c46d4069e5b0f605621be58e
Kevin Harold Gutierrez Ramirez <harold.dev1993@gmail.com>
2022-07-15 08:52:48 -0500

    agregando fetch a callenge.js

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,7 +1,7 @@
 //Clase7: XMLHTTP Request
 //XMLHTTP Request: Permite manejar de mejor forma las peticiones y es fundamental del API de JavaScript. Permite hacer peticiones a recursos XML o JSON.
 
-const XMLHttpRequest = require('xmlhttprequest');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 const API = 'https://api.escuelajs.co/api/v1';
 
 function fetchData(urlApi, callback) {
@@ -18,11 +18,24 @@ function fetchData(urlApi, callback) {
             //200 muestra cuando la solicitud ha sido correcta.
             if (xhttp.status === 200) {
                 callback(null, JSON.parse(xhttp.responseText))
+            } else {
+                const error = new Error('Error' + urlApi);
+                return callback(error, null);
             }
-        } else {
-            const error = new Error('Error' + urlApi);
-            return callback(error, null);
-        }
+        } 
     }
     xhttp.send(); //acá estamos pidiendo que se ejecute el ajax y también le estamos mandando datos
 }
+
+fetchData(`${API}/products`, function(error1, data1) {
+    if (error1) return console.error(error1);
+    fetchData(`${API}/products/${data1[0].id}`, function(error2, data2) {
+        if (error2) return console.error(error2);
+        fetchData(`${API}/categories/${data2.category.id}`, function(error3, data3) {
+            if (error3) return console.error(error3);
+            console.log(data1[0]);
+            console.log(data2.title);
+            console.log(data3.name);
+        })
+    })
+})

486e3a1a2b0eb9238b4898c05098cdfcc0e537e2
Kevin Harold Gutierrez Ramirez <harold.dev1993@gmail.com>
2022-07-15 17:41:22 -0500

    no entiendo ajax helpme fredyvega

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -7,7 +7,7 @@ const API = 'https://api.escuelajs.co/api/v1';
 function fetchData(urlApi, callback) {
     let xhttp = new XMLHttpRequest();
 
-    xhttp.open('GET, urlApi, true');
+    xhttp.open('GET', urlApi, true);
     xhttp.onreadystatechange = function (event) {
         if (xhttp.readyState === 4) {
             //0 indica que no se ha inicializado
@@ -31,11 +31,11 @@ fetchData(`${API}/products`, function(error1, data1) {
     if (error1) return console.error(error1);
     fetchData(`${API}/products/${data1[0].id}`, function(error2, data2) {
         if (error2) return console.error(error2);
-        fetchData(`${API}/categories/${data2.category.id}`, function(error3, data3) {
+        fetchData(`${API}/categories/${data2?.category?.id}`, function(error3, data3) {
             if (error3) return console.error(error3);
             console.log(data1[0]);
             console.log(data2.title);
             console.log(data3.name);
-        })
-    })
-})
+        });
+    });
+});

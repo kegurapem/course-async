71b2fe503e01d9409c7f777b8dd60b4fe72208bc
Kevin Harold Gutierrez Ramirez <harold.dev1993@gmail.com>
2022-07-22 16:33:27 -0500

    entend[i y comprend[[i AJAX, necesito practicar m[as

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -2,8 +2,7 @@
 //XMLHTTP Request: Permite manejar de mejor forma las peticiones y es fundamental del API de JavaScript. Permite hacer peticiones a recursos XML o JSON.
 
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-
-
+const API = 'https://api.escuelajs.co/api/v1'
 
 function fetchData(urlApi, callback) {
     let xhttp = new XMLHttpRequest();
@@ -32,7 +31,7 @@ fetchData(`${API}/products`, function(error1, data1) {
     if (error1) return console.error(error1);
     fetchData(`${API}/products/${data1[0].id}`, function(error2, data2) {
         if (error2) return console.error(error2);
-        fetchData(`${API}/categories/${data2?.category?.id}`, function(error3, data3) {
+        fetchData(`${API}/categories/${data2?.category?.id}/`, function(error3, data3) {
             if (error3) return console.error(error3);
             console.log(data1[0]);
             console.log(data2.title);
